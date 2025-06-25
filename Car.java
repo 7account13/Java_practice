4f7a0576f1587f637fbfd4fc38750b0a4a58451a
7account13 <dineshsenthil2005@gmail.com>
2025-06-25 18:59:03 +0530

    18:58

--- Car.java
+++ Car.java
@@ -1,19 +1,34 @@
+
+
+
 public class Car{
     String name;
     int speed;
+    Car(String n,int s)
+    {
+        this.name=n;
+        this.speed=s;
+
+    }
     void drive(){
         System.out.println("The "+name+" is driving at "+speed+" km/hr");
 
     }
 public static void main(String[] args) {
-    Car ferrari=new Car();
-    ferrari.name="ferrari";
-    ferrari.speed=220;
+   Car ferrari=new Car("Ferrari",220);
+    
     ferrari.drive();
 
-    Car Tata = new Car();
-    Tata.name="tata nano";
-    Tata.speed=45;
+    Car Tata = new Car("TATA",70);
+ 
     Tata.drive();
+    
+    ev e=new ev ("Tata nexon",90);
+    e.drive();
 }
+}
+class ev extends Car{
+    ev(String n,int s){
+        super(n,s);
+    }
 }
---
+++ Story.java
@@ -0,0 +1,31 @@
+
+/* abstraction and encapsulation*/
+abstract class Story{
+    String genre;
+
+    Story(String g){
+        this.genre=g;
+    }
+abstract void narrate();
+void intro(){
+    System.out.println("This is intro of the Book for genre :"+genre);
+
+}
+public static void main(String[] args) {
+    DarkRomanceStory r=new DarkRomanceStory("romance");
+    r.intro();
+    r.narrate();
+}
+}
+
+class DarkRomanceStory extends Story{
+    DarkRomanceStory(String g){
+        super(g);
+    }
+    @Override
+    void narrate(){
+        System.out.println("It looks like someone snatched the stars and thow that into your eyes");
+
+    }
+
+}
